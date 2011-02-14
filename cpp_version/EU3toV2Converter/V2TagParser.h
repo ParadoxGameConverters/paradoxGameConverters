375fa55a3a05f0fe59874bf6cb9cb2af9006fad4
idhrendur <idhrendur@hotmail.com>
2011-02-14 00:12:38 +0000

    Files needed for the new Tag conversion method.

---
+++ cpp_version/EU3toV2Converter/V2TagParser.h
@@ -0,0 +1,12 @@
+#ifndef V2TAGPARSER_H_
+#define V2TAGPARSER_H_
+
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
+
+vector<string> parseV2Tags(ifstream &input);
+
+
+#endif // V2TAGPARSER_H_
