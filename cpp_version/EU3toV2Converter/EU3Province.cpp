277b458a1f0005f8794fc79feff8177116e5f24a
idhrendur <idhrendur@hotmail.com>
2011-02-21 01:55:55 +0000

    Imports EU3 province population.

--- cpp_version/EU3toV2Converter/EU3Province.cpp
+++ cpp_version/EU3toV2Converter/EU3Province.cpp
@@ -15,6 +15,16 @@ void EU3Province::init(Object* obj) {
 		owner = ownerObjs[0]->getLeaf();
 		owner = owner.substr(1,3);
 	}
+	vector<Object*> popObj = obj->getValue("citysize");
+	if (popObj.size() > 0)
+	{
+		population	= atoi( (popObj)[0]->getLeaf().c_str() );
+	}
+	else
+	{
+		vector<Object*> popObj	= obj->getValue("native_size");
+		population					= atoi( (popObj)[0]->getLeaf().c_str() );
+	}
 }
 
 
--- cpp_version/EU3toV2Converter/EU3Province.h
+++ cpp_version/EU3toV2Converter/EU3Province.h
@@ -17,6 +17,7 @@ class EU3Province {
 		//cores
 		//culture
 		//religion
+		int		population;
 };
 
 
