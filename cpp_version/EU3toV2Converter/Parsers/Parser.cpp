3efdd922614882f93bc962961c1e37ba10b97595
idhrendur <idhrendur@hotmail.com>
2011-05-04 01:11:30 +0000

    Parser now handles single-line top-level objects.

--- cpp_version/EU3toV2Converter/Parsers/Parser.cpp
+++ cpp_version/EU3toV2Converter/Parsers/Parser.cpp
@@ -2,21 +2,19 @@
 #include "Object.h"
 
 Object* Parser::topLevel = NULL;  
-std::vector<Object*> stack; 
-std::vector<Object*> objstack; 
+vector<Object*> stack; 
+vector<Object*> objstack; 
 
 void initParser()
 {
-   Parser::topLevel = new Object("topLevel");
+	Parser::topLevel = new Object("topLevel");
 }
 
-int trim (std::string& str)
+bool trim(string& str, int& count)	// Returns net number of braces opened by str, and trims leading and trailing whitespace.
 {
-  // Returns net number of braces opened by str, and trims leading and trailing whitespace.
-
 	const char*		s			= str.c_str();
-	int				ret		= 0;
 	unsigned int	strSize	= str.size();
+	bool				opened	= false;
 
 	if (0 == strSize)
 	{
@@ -40,11 +38,12 @@ int trim (std::string& str)
 		}
 		if ('{' == s[i])
 		{
-			++ret;
+			++count;
+			opened = true;
 		}
 		else if ('}' == s[i])
 		{
-			--ret;
+			--count;
 		}
 	}
 
@@ -84,168 +83,204 @@ int trim (std::string& str)
 	}
 	if (fcomment < last)
 	{
-		last = fcomment-1;
+		last = fcomment - 1;
 	}
 
 	str = str.substr(first, last - first + 1); 
-	return ret; 
+	return opened; 
 }
 
-void readFile (std::ifstream& read) {
-  clearStack(); 
-  int openBraces = 0;
-  std::string currObject;  
-  bool topLevel = true; 
+void readFile (ifstream& read)
+{
+	clearStack();
 
-  while (read.good()) {
-    std::string buffer;
-    std::getline(read, buffer);
+	int openBraces = 0;
+	string currObject;
+	bool topLevel = true;
+	while (read.good())
+	{
+		string buffer;
+		getline(read, buffer);
 
-    int currBraces = trim(buffer); 
-    openBraces += currBraces; 
-    currObject += " " + buffer; 
+		bool opened = trim(buffer, openBraces);
+		currObject		+= " " + buffer;
 
-    if (openBraces > 0) {
-      topLevel = false;
-      continue; 
-    }
-    if (currObject == "") continue; 
+		if (openBraces > 0)
+		{
+			topLevel = false;
+			continue;
+		}
 
-    // Don't try to end an object that hasn't started properly;
-    // accounts for such constructions as
-    // object = 
-    // { 
-    // where openBraces is zero after the first line of the object. 
-    // Not a problem for non-top-level objects since these will have
-    // nonzero openBraces anyway. 
-    if (topLevel) continue; 
+		if (currObject == "")
+		{
+			continue;
+		}
 
-    openBraces = 0; 
-    makeObject(currObject);
-    currObject.clear(); 
-    topLevel = true; 
-  }
+		// Don't try to end an object that hasn't started properly;
+		// accounts for such constructions as
+		// object = 
+		// { 
+		// where openBraces is zero after the first line of the object. 
+		// Not a problem for non-top-level objects since these will have
+		// nonzero openBraces anyway.
+		// But don't continue if the object was all on one line.
+		if (topLevel && !opened)
+		{
+			continue; 
+		}
 
-  trim(currObject); 
-  // MAX: Ignore this 
-  /*
-  if (currObject.size() > 0) {
-    std::cout << "Warning: Unable to parse file; problem is with \"" << currObject << "\"" << std::endl; 
-    std::cout << "Aborting, unable to parse file" << std::endl; 
-    abort();
-  }
-  */
-}
+		openBraces = 0; 
+		makeObject(currObject);
+		currObject.clear(); 
+		topLevel = true; 
+	}
 
-
-void clearStack () {
-  if (0 < stack.size()) std::cout << "Warning: Clearing stack size " 
-				  << stack.size() 
-				  << ". This should not happen in normal operation." 
-				  << std::endl; 
-  for (std::vector<Object*>::iterator i = stack.begin(); i != stack.end(); ++i) {
-    //std::cout << (*i)->getKey() << std::endl; 
-    std::cout << (*(*i)) << std::endl; 
-  }
-  stack.clear();
-}
-
-void setLHS (char const* first, char const* last) {
-  std::string key(first, last); 
-  //std::cout << "Setting lefthand side " << key << std::endl; 
-  Object* p = new Object(key); 
-  if (0 == stack.size()) {
-    Parser::topLevel->setValue(p);
-  }
-  stack.push_back(p); 
-}
-
-void pushObj (char const* first, char const* last) {
-  std::string key("objlist");
-  Object* p = new Object(key); 
-  p->setObjList(); 
-  objstack.push_back(p); 
+	trim(currObject, openBraces); 
+	// MAX: Ignore this 
+	/*
+	if (currObject.size() > 0) {
+		std::cout << "Warning: Unable to parse file; problem is with \"" << currObject << "\"" << std::endl; 
+		std::cout << "Aborting, unable to parse file" << std::endl; 
+		abort();
+	}
+	*/
 }
 
 
-void setRHSleaf (char const* first, char const* last) {
-  std::string val(first, last); 
-  Object* l = stack.back();
-  stack.pop_back(); 
-  l->setValue(val); 
-  if (0 < stack.size()) {
-    Object* p = stack.back(); 
-    p->setValue(l); 
-  }
-}
-
-void setRHStaglist (char const* first, char const* last) {
-  Object* l = stack.back();
-  stack.pop_back(); 
-
-  std::string val(first, last); 
-  std::string tag;
-  bool stringmode = false; 
-  for (unsigned int i = (val[0] == '{') ? 1 : 0; i < val.size(); ++i) {
-    if ((val[i] == ' ') && (0 == tag.size())) continue; 
-    if (val[i] == '"') {
-      tag.push_back(val[i]); 
-      if (stringmode) {
-	l->addToList(tag); 
-	tag.clear(); 
-      }
-      stringmode = !stringmode;
-      continue;
-    }
-    if (((val[i] == ' ') && (!stringmode)) || (val[i] == '}')) {
-      if (tag.size() > 0) l->addToList(tag); 
-      tag.clear(); 
-      continue;
-    }
-    tag.push_back(val[i]); 
-  }
-
-  if (0 < stack.size()) {
-    Object* p = stack.back(); 
-    p->setValue(l); 
-  }
-}
-
-void setRHSobject (char const* first, char const* last) {
-  // No value is set, a bunch of leaves have been defined. 
-  Object* l = stack.back();
-  stack.pop_back(); 
-  if (0 < stack.size()) {
-    Object* p = stack.back(); 
-    p->setValue(l); 
-  }
-}
-
-void setRHSobjlist (char const* first, char const* last) {
-  if (0 < stack.size()) {
-    Object* p = stack.back(); 
-    p->setValue(objstack); 
-  }
-  stack.pop_back(); 
-  objstack.clear(); 
+void clearStack()
+{
+	if (0 < stack.size())
+	{
+		std::cout << "Warning: Clearing stack size " << stack.size() << ". This should not happen in normal operation." << std::endl;
+	}
+	for (std::vector<Object*>::iterator i = stack.begin(); i != stack.end(); ++i)
+	{
+		//std::cout << (*i)->getKey() << std::endl;
+		std::cout << (*(*i)) << std::endl;
+	}
+	stack.clear();
 }
 
 
-bool makeObject (std::string& command) {
-  //std::cout << "Entering makeObject with " << command << std::endl; 
-  static Parser p;
-  //BOOST_SPIRIT_DEBUG_GRAMMAR(p);
-  boost::spirit::classic::parse_info<> result = boost::spirit::classic::parse(command.c_str(), p, boost::spirit::classic::nothing_p);
-  if (result.full) {
-    //std::cout << "Matched " << command << std::endl; 
-    // Do stuff with full command
+void setLHS(char const* first, char const* last)
+{
+	string key(first, last);
+	//cout << "Setting lefthand side " << key << endl;
+	Object* p = new Object(key);
+	if (0 == stack.size())
+	{
+		Parser::topLevel->setValue(p);
+	}
+	stack.push_back(p);
+}
 
-    //command.clear(); 
-    return true; 
-  }
+void pushObj(char const* first, char const* last)
+{
+	string key("objlist");
+	Object* p = new Object(key); 
+	p->setObjList(); 
+	objstack.push_back(p); 
+}
 
-  //std::cout << "No match in " << command << " stopped at \"" << result.stop << "\"" << std::endl;
-  return false; 
+
+void setRHSleaf(char const* first, char const* last)
+{
+	string val(first, last); 
+	Object* l = stack.back();
+	stack.pop_back(); 
+	l->setValue(val); 
+	if (0 < stack.size())
+	{
+		Object* p = stack.back(); 
+		p->setValue(l); 
+	}
+}
+
+void setRHStaglist(char const* first, char const* last)
+{
+	Object* l = stack.back();
+	stack.pop_back(); 
+
+	string val(first, last); 
+	string tag;
+	bool stringmode = false; 
+	for (unsigned int i = (val[0] == '{') ? 1 : 0; i < val.size(); ++i)
+	{
+		if ((val[i] == ' ') && (0 == tag.size()))
+		{
+			continue;
+		}
+		if (val[i] == '"')
+		{
+			tag.push_back(val[i]); 
+			if (stringmode)
+			{
+				l->addToList(tag); 
+				tag.clear(); 
+			}
+			stringmode = !stringmode;
+			continue;
+		}
+		if (((val[i] == ' ') && (!stringmode)) || (val[i] == '}'))
+		{
+	      if (tag.size() > 0)
+			{
+				l->addToList(tag); 
+			}
+			tag.clear(); 
+			continue;
+		}
+		tag.push_back(val[i]); 
+	}
+
+	if (0 < stack.size())
+	{
+		Object* p = stack.back(); 
+		p->setValue(l); 
+	}
+}
+
+void setRHSobject(char const* first, char const* last)
+{
+	// No value is set, a bunch of leaves have been defined. 
+	Object* l = stack.back();
+	stack.pop_back(); 
+	if (0 < stack.size())
+	{
+		Object* p = stack.back(); 
+		p->setValue(l); 
+	}
+}
+
+void setRHSobjlist(char const* first, char const* last)
+{
+	if (0 < stack.size())
+	{
+		Object* p = stack.back(); 
+		p->setValue(objstack); 
+	}
+	stack.pop_back(); 
+	objstack.clear(); 
+}
+
+
+bool makeObject(std::string& command)
+{
+	//std::cout << "Entering makeObject with " << command << std::endl;
+	static Parser p;
+	//BOOST_SPIRIT_DEBUG_GRAMMAR(p);
+	parse_info<> result = parse(command.c_str(), p, nothing_p);
+	if (result.full)
+	{
+		//std::cout << "Matched " << command << std::endl;
+		// Do stuff with full command
+
+		//command.clear();
+		return true;
+	}
+
+	//std::cout << "No match in " << command << " stopped at \"" << result.stop << "\"" << std::endl;
+	return false;
 }
 
 
