4c5509e135840091a9c339be576fcce294e4f9d0
idhrendur <idhrendur@hotmail.com>
2012-01-15 22:37:23 +0000

    Updated build.bat.

--- cpp_version/EU3toV2Converter/build.bat
+++ cpp_version/EU3toV2Converter/build.bat
@@ -2,11 +2,11 @@ del *.zip /q
 
 call "%VSTUDIO_LOC%\VC\vcvarsall.bat"
 
-del release /q
-rmdir release
-
 del Release /q
 rmdir Release
+
+del ReleaseIntermediate/q
+rmdir ReleaseIntermediate
 
 msbuild.exe EU3toV2Converter.sln /p:Configuration=Release
 cd Release
