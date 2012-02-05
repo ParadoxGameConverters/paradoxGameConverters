019339913a78eda16a83a80bbbe280c57fd3794d
dtremenak <devnull@localhost>
2012-02-05 23:01:34 +0000

    Toss these files in too, since I accidentally committed the changes that require them.  Adds a configuration dialog.  Needs work, but probably no more broken than the rest of the frontend.

---
+++ cs_frontend/EU3ToVic2_Frontend/ConfigurationForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EU3ToVic2_Frontend
+{
+    public partial class ConfigurationForm : Form
+    {
+        public ConfigurationForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
