3cb1030a70a148379050987d5bc51d50769bbd88
dtremenak <devnull@localhost>
2012-05-30 05:45:08 +0000

    allow the parser to handle empty brace sets, since EU3 seems to drop them in occasionally.  also, some parser documentation.

--- cpp_version/EU3toV2Converter/Parsers/Parser.cpp
+++ cpp_version/EU3toV2Converter/Parsers/Parser.cpp
@@ -33,26 +33,55 @@ template <typename Iterator>
 struct Parser : public qi::grammar<Iterator, SkipComment<Iterator> > {
 	static Object* topLevel; 
 
+	// leaf: either left or right side of assignment.  unquoted keyword.
+	// example: leaf
 	qi::rule<Iterator, string(), SkipComment<Iterator> >	leaf;
+
+	// taglist: a grouping of anonymous (rhs) leaves or strings
+	// examples: { TAG TAG TAG } or { "string" "string" TAG }
 	qi::rule<Iterator, vector<string>(), SkipComment<Iterator> >	taglist;
+
+	// assign: assignment
+	// examples: lhs = rhs or lhs = { lhs = rhs }
 	qi::rule<Iterator, SkipComment<Iterator> >	assign;
+
+	// object: a grouping of assignments
+	// example: object = { leaf = "string" }
 	qi::rule<Iterator, SkipComment<Iterator> >	object;
+
+	// objlist: a grouping of anonymous (rhs) objects
+	// example: objlist = { { leaf = "string } { leaf = leaf } }
 	qi::rule<Iterator, SkipComment<Iterator> >	objlist;
+
+	// str: a quoted literal string.  may include extended and/or reserved characters.
+	// example: "I am a string."
 	qi::rule<Iterator, string()>	str;
+
+	// tolleaf: a tolerant leaf.  may include extended and other unreserved characters.  rhs only.
+	// example: leaves with accents (names, for instance).
 	qi::rule<Iterator, string(), SkipComment<Iterator> >	tolleaf;
 
-	Parser() : Parser::base_type(assign)
+	// braces: a stray set of empty rhs braces (without an lhs)
+	// EU3 seems to do this for certain decision mods.
+	qi::rule<Iterator, SkipComment<Iterator> >	braces;
+
+	// root: evaluation starts here.  objects recurse to root.
+	qi::rule<Iterator, SkipComment<Iterator> >	root;
+
+	Parser() : Parser::base_type(root)
 	{
+		braces	= lit('{') >> *(iso8859_1::space) >> lit('}');
 		str     = lexeme[lit('"') >> raw[*(~iso8859_1::char_('"'))] >> lit('"')];
 		tolleaf = raw[+(~iso8859_1::char_("\"{}= \t\r\n"))];
 		leaf    = raw[+(iso8859_1::alnum | iso8859_1::char_("-._:"))];
 		taglist = lit('{') >> omit[*(iso8859_1::space)] >> lexeme[( ( str | skip[tolleaf] ) % *(iso8859_1::space) )] >> omit[*(iso8859_1::space)] >> lit('}');
-		object  = raw[lit('{') >> *(assign) >> *(iso8859_1::space) >> lit('}')];
+		object  = raw[lit('{') >> *(root) >> *(iso8859_1::space) >> lit('}')];
 		objlist = raw[lit('{') >> *( *(iso8859_1::space) >> object[&pushObj] ) >> *(iso8859_1::space) >> lit('}')];
-		assign  = raw[+((*(iso8859_1::space) >> ( leaf[&setLHS] | str[&setLHS]) >> *(iso8859_1::space) >> lit('=')
+		assign  = raw[(*(iso8859_1::space) >> ( leaf[&setLHS] | str[&setLHS]) >> *(iso8859_1::space) >> lit('=')
 			>> *(iso8859_1::space) 
 			>> ( leaf[&setRHSleaf] | str[&setRHSleaf] | taglist[&setRHStaglist] | objlist[&setRHSobjlist] | object[&setRHSobject] ) 
-			>> *(iso8859_1::space)))];
+			>> *(iso8859_1::space))];
+		root	= +(assign | braces);
 
 		str.name("str");
 		leaf.name("leaf");
@@ -60,6 +89,8 @@ struct Parser : public qi::grammar<Iterator, SkipComment<Iterator> > {
 		object.name("object");
 		objlist.name("objlist");
 		assign.name("assign");
+		braces.name("braces");
+		root.name("root");
 
 		if (debugme)
 		{
@@ -69,6 +100,8 @@ struct Parser : public qi::grammar<Iterator, SkipComment<Iterator> > {
 			debug(object);
 			debug(objlist);
 			debug(assign);
+			debug(braces);
+			debug(root);
 		}
 	}
 };
