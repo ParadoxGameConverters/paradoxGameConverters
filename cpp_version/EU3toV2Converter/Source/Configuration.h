72da712c723b5c23d9ae5188834ba703f33af020
idhrendur <idhrendur@hotmail.com>
2012-07-19 01:00:29 +0000

    Refactor Configuration

--- cpp_version/EU3toV2Converter/Source/Configuration.h
+++ cpp_version/EU3toV2Converter/Source/Configuration.h
@@ -8,61 +8,35 @@ using namespace std;
 
 class Configuration // Singleton
 {
-public:
-	Configuration();
+	public:
+		Configuration();
 
-	static date getStartDate()
-	{
-		return getInstance()->startDate;
-	}
+		static date getStartDate()			const { return getInstance()->startDate; }
+		static double getMaxLiteracy()	const { return getInstance()->maxLiteracy; }
+		static string getV2Path()			const { return getInstance()->V2Path; }
+		static string getEU3Path()			const { return getInstance()->EU3Path;	}
+		static string getEU3Gametype()	const { return getInstance()->EU3gametype; }
+		static string getV2Gametype()		const { return getInstance()->V2gametype;	}
+		static string getRemovetype()		const { return getInstance()->removetype;	}
+		static Configuration* getInstance()
+		{
+			if (instance == NULL)
+			{
+				instance = new Configuration();
+			}
+			return instance;
+		}
 
-	static double getMaxLiteracy()
-	{
-		return getInstance()->maxLiteracy;
-	}
+	private:
+		static Configuration* instance;
 
-	static string getV2Path()
-	{
-		return getInstance()->V2Path;
-	}
-
-	static string getEU3Path()
-	{
-		return getInstance()->EU3Path;
-	}
-
-	static string getEU3Gametype()
-	{
-		return getInstance()->EU3gametype;
-	}
-
-	static string getV2Gametype()
-	{
-		return getInstance()->V2gametype;
-	}
-
-	static string getRemovetype()
-	{
-		return getInstance()->removetype;
-	}
-
-	static Configuration* getInstance()
-	{
-		if (instance == NULL)
-			instance = new Configuration();
-		return instance;
-	}
-
-private:
-	static Configuration* instance;
-
-	date		startDate;
-	double	maxLiteracy;
-	string	V2Path;
-	string	EU3Path;
-	string	EU3gametype;
-	string	V2gametype;
-	string	removetype;
+		date		startDate;
+		double	maxLiteracy;
+		string	V2Path;
+		string	EU3Path;
+		string	EU3gametype;
+		string	V2gametype;
+		string	removetype;
 };
 
 #endif // CONFIGURATION_H_
