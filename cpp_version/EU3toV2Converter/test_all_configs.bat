f69dd33d5c1b75cc53fe73c670f82f9b2c45027e
idhrendur <idhrendur@hotmail.com>
2012-07-26 21:29:04 +0000

    Update automated test batch files

--- cpp_version/EU3toV2Converter/test_all_configs.bat
+++ cpp_version/EU3toV2Converter/test_all_configs.bat
@@ -1,14 +1,25 @@
 echo off
 
 copy Release\configuration.txt Release\configuration-backup.txt
-del Release\configuration.txt
+copy Release\country_mappings.txt Release\country_mappings-backup.txt
+copy Release\cultureMap.txt Release\cultureMap-backup.txt
+copy Release\religionMap.txt Release\religionMap-backup.txt
+copy Release\unions.txt Release\unions-backup.txt
 
 for /f %%p in ('dir /b testresults\') do call del testresults\%%p /q
 for /f %%p in ('dir /b testresults\') do call rmdir testresults\%%p
 del testResults /q
 rmdir testresults
 
-for /f %%n in ('dir /b TestConfigurations\*.txt') do call test_all_saves.bat "%%n"
+for /f %%n in ('dir /b TestConfigurations\') do call test_all_saves.bat "%%n"
 
 copy Release\configuration-backup.txt Release\configuration.txt
+copy Release\country_mappings-backup.txt copy Release\country_mappings.txt
+copy Release\cultureMap-backup.txt copy Release\cultureMap.txt
+copy Release\religionMap-backup.txt copy Release\religionMap.txt
+copy Release\unions-backup.txt copy Release\unions.txt
 del Release\configuration-backup.txt /q
+del Release\country_mappings-backup.txt /q
+del Release\cultureMap-backup.txt /q
+del Release\religionMap-backup.txt /q
+del Release\unions-backup.txt /q
--- cpp_version/EU3toV2Converter/test_all_saves.bat
+++ cpp_version/EU3toV2Converter/test_all_saves.bat
@@ -4,7 +4,11 @@ echo Preparing to test the %m% config.
 
 mkdir testresults\%m%
 
-copy TestConfigurations\%m%.txt Release\configuration.txt
+copy TestConfigurations\%m%\configuration.txt Release\configuration.txt
+copy TestConfigurations\%m%\country_mappings.txt Release\country_mappings.txt
+copy TestConfigurations\%m%\cultureMap.txt Release\cultureMap.txt
+copy TestConfigurations\%m%\religionMap.txt Release\religionMap.txt
+copy TestConfigurations\%m%\unions.txt Release\unions.txt
 
 copy test.bat Release\test.bat
 cd Release
