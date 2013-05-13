35d88e4823a42910da27cb8fa1cc27f789536a8a
idhrendur <idhrendur@hotmail.com>
2013-05-13 16:04:44 -0700

    Update testing scripts for new mod stuff

--- test_all_configs.bat
+++ test_all_configs.bat
@@ -6,14 +6,14 @@ copy Release\religion_mappings.txt Release\religion_mappings-backup.txt
 
 for /f %%p in ('dir /b testresults\') do call del testresults\%%p /q
 for /f %%p in ('dir /b testresults\') do call rmdir testresults\%%p
-del testResults /q
-rmdir testresults
+del testResults /s /q
+rmdir testresults /s /q
 
 for /f %%n in ('dir /b TestConfigurations\') do call test_all_saves.bat "%%n"
 
 copy Release\configuration-backup.txt Release\configuration.txt
 copy Release\culture_mappings-backup.txt copy Release\culture_mappings.txt
 copy Release\religion_mappings-backup.txt copy Release\religion_mappings.txt
-del Release\configuration-backup.txt /q
-del Release\culture_mappings-backup.txt /q
-del Release\religion_mappings-backup.txt /q
+del Release\configuration-backup.txt /s /q
+del Release\culture_mappings-backup.txt /s /q
+del Release\religion_mappings-backup.txt /s /q
