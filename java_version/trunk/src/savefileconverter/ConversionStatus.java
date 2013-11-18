2d9fff097f9c1cd69aa73f029ad23ee777f1cdbe
idhrendur <idhrendur@hotmail.com>
2013-11-17 16:41:58 -0800

    Remove old java version

--- java_version/trunk/src/savefileconverter/ConversionStatus.java
+++
@@ -1,9 +0,0 @@
-package savefileconverter;
-
-public interface ConversionStatus {
-	public void conversionEnded(boolean success);
-	public void printInfo(String line);
-	public void printError(String line);
-	public void conversionProgress(String name, int task, int numTasks);
-	public void taskProgress(float fraction);
-}
