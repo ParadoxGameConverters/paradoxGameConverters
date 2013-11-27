50e5ba60c0421390f5c730f818e11044d6b26f87
kwsdev <kwsdev@fastmail.fm>
2013-11-27 20:39:41 +0100

    Re-added build script in ui project post-build event Added missing DateParseHelper

---
+++ cpp_version/EU3toV2Converter/UI/Helpers/DateParseHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Converter.UI.Helpers
+{
+    public static class DateParseHelper
+    {
+        /// <summary>
+        /// Tries the parse double.
+        /// </summary>
+        /// <param name="s">The arguments.</param>
+        /// <param name="result">The result.</param>
+        /// <returns></returns>
+        public static bool TryParseDate(string s, string dateFormat, out DateTime result)
+        {
+            return DateTime.TryParseExact(s, dateFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out result);
+        }
+
+        /// <summary>
+        /// Tries the parse double.
+        /// </summary>
+        /// <param name="s">The arguments.</param>
+        /// <param name="result">The result.</param>
+        /// <returns></returns>
+        public static bool TryParseDate(string s, out DateTime result)
+        {
+            return DateTime.TryParse(s, CultureInfo.InvariantCulture, DateTimeStyles.None, out result);
+        }
+    }
+}
