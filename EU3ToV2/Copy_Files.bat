84b692f1dfbdaccd40bbb70c55322874ef4d3863
idhrendur <idhrendur@hotmail.com>
2013-12-27 13:04:01 -0800

    Automate changelog

--- EU3ToV2/Copy_Files.bat
+++ EU3ToV2/Copy_Files.bat
@@ -3,7 +3,6 @@ copy "data files\readme.txt" "release\readme.txt"
 copy "data files\DW_province_mappings.txt" "release\DW_province_mappings.txt"
 copy "data files\HttT_province_mappings.txt" "release\HttT_province_mappings.txt"
 copy "data files\IN_province_mappings.txt" "release\IN_province_mappings.txt"
-copy "data files\changelog.txt" "release\changelog.txt"
 copy "data files\cultureMap.txt" "release\cultureMap.txt"
 copy "data files\religionMap.txt" "release\religionMap.txt"
 copy "data files\changeLog.txt" "release\changeLog.txt"
@@ -18,3 +17,8 @@ copy "data files\leader_traits.txt" "release\leader_traits.txt"
 copy "data files\merge_nations.txt" "release\merge_nations.txt"
 copy "data files\configuration.txt" "release\configuration.txt"
 copy "data files\blocked_tech_schools.txt" "release\blocked_tech_schools.txt"
+
+rem hg log > Release/log.txt
+hg log --template "Change:\t\t{rev}: {node}\nAuthor:\t\t{author}\nDescription:\t{desc}\nDate:\t\t{date|isodate}\nBranch:\t\t{branch}\n***\n" > Release/log.txt
+(for /f "delims=" %%i in (release/log.txt) do @echo %%i)>release/changelog.txt
+del release\log.txt
