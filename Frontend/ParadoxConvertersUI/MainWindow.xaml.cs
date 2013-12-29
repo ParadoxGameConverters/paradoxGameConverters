82372992c4ed44224a8d5b728fc8bb3337bb479d
kwsdev <kwsdev@fastmail.fm>
2013-12-29 18:37:12 +0100

    Initial frontend checkin.

---
+++ Frontend/ParadoxConvertersUI/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ParadoxConverters.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        #region [ Fields ]
+
+        /// <summary>
+        /// Backing store for the current view
+        /// </summary>
+        private UserControl currentView;
+
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion [ Fields ]
+
+        #region [ Constructors ]
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion [ Constructors ]
+
+        #region [ Properties ]
+
+        public UserControl FrameView
+        {
+            get
+            {
+                return this.currentView;
+            }
+
+            set
+            {
+                if (this.currentView == value)
+                {
+                    return;
+                }
+
+                this.currentView = value;
+                this.RaisePropertyChanged("CurrentView");
+            }
+        }
+
+        #endregion [ Constructors ]
+
+        #region [ Methods ]
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Windows.FrameworkElement.Initialized" /> event. This method is invoked whenever <see cref="P:System.Windows.FrameworkElement.IsInitialized" /> is set to true internally.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.Windows.RoutedEventArgs" /> that contains the event data.</param>
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+
+            // Set the frontend culture to the user's current culture.
+            FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+
+            //this.FrameView = new WelcomeView();
+            this.DataContext = this;
+        }
+
+        /// <summary>
+        /// Raises the property changed.
+        /// </summary>
+        /// <param name="propertyName">Name of the property.</param>
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion [ Methods ]
+    }
+}
