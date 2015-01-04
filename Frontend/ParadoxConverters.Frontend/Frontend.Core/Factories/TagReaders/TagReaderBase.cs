d035c1df1b8ad6a69db8fac7035cec345b9e33a9
kwsdev <kwsdev@fastmail.fm>
2015-01-04 19:13:15 +0100

    CodeAnalysis cleanup: Fixed CA2241.

--- Frontend/ParadoxConverters.Frontend/Frontend.Core/Factories/TagReaders/TagReaderBase.cs
+++ Frontend/ParadoxConverters.Frontend/Frontend.Core/Factories/TagReaders/TagReaderBase.cs
@@ -150,7 +150,7 @@ namespace Frontend.Core.Factories.TagReaders
 
         private void LogExistenceError(IAlternativePath nonExistingAlternativePath, string tagName, string friendlyName)
         {
-            var errorMessageText = string.Format("The path to {0} could not be detected, and need to be set manually here in the frontend. Was expecting it to exist at: ", friendlyName, nonExistingAlternativePath.Path);
+            var errorMessageText = string.Format("The path to {0} could not be detected, and need to be set manually here in the frontend. Was expecting it to exist at:", friendlyName);
             
             this.EventAggregator.PublishOnUIThread(new LogEntry(errorMessageText, LogEntrySeverity.Info, LogEntrySource.UI, nonExistingAlternativePath.Path));
         }
