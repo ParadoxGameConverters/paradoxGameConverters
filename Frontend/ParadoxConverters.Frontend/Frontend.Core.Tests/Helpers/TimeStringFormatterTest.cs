bd3e1e15e35a195a7ae05731b3af0fd1f5a29c3d
kwsdev <kwsdev@fastmail.fm>
2015-01-31 17:01:20 +0100

    Added a couple of unit test files. I blame the lack of ignore file before this point.

---
+++ Frontend/ParadoxConverters.Frontend/Frontend.Core.Tests/Helpers/TimeStringFormatterTest.cs
@@ -0,0 +1,32 @@
+﻿using Frontend.Core.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frontend.Core.Tests.Helpers
+{
+    [TestClass]
+    public class TimeStringFormatterTest : UnitTestBase
+    {
+        [TestMethod]
+        public void MinutesSecondTest()
+        {
+            var expected = "03m 02s";
+            var timesspan = new TimeSpan(0, 3, 2);
+            var actual = TimeStringFormatter.BuildTimeString(timesspan);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DualDigitMinutesSecondsTest()
+        {
+            var expected = "12m 24s";
+            var timespan = new TimeSpan(0, 12, 24);
+            var actual = TimeStringFormatter.BuildTimeString(timespan);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
