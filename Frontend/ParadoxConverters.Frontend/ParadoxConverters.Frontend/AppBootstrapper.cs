176ba3a33a5e4b97ba323cf6be3b44699818101e
kwsdev <kwsdev@fastmail.fm>
2015-02-08 19:17:04 +0100

    Added NDepend files to ignore filter. Tabifying/style fixes.

--- Frontend/ParadoxConverters.Frontend/ParadoxConverters.Frontend/AppBootstrapper.cs
+++ Frontend/ParadoxConverters.Frontend/ParadoxConverters.Frontend/AppBootstrapper.cs
@@ -1,63 +1,66 @@
 namespace Frontend.Client
 {
-    using System;
-    using System.Linq;
-    using System.Collections.Generic;
-    using Caliburn.Micro;
-    using Frontend.Core.ViewModels;
-    using Frontend.Core.ViewModels.Interfaces;
-    using System.Reflection;
-    using Frontend.Core.Logging;
-    using System.Windows;
+	using System;
+	using System.Linq;
+	using System.Collections.Generic;
+	using Caliburn.Micro;
+	using Frontend.Core.ViewModels;
+	using Frontend.Core.ViewModels.Interfaces;
+	using System.Reflection;
+	using Frontend.Core.Logging;
+	using System.Windows;
+    using System.Windows.Threading;
 
-    public class AppBootstrapper : BootstrapperBase
-    {
-        private SimpleContainer container;
-        private IEventAggregator eventAggregator;
+	public class AppBootstrapper : BootstrapperBase
+	{
+		private SimpleContainer container;
+		private IEventAggregator eventAggregator;
 
-        public AppBootstrapper()
-        {
-            this.StartRuntime();
-        }
+		public AppBootstrapper()
+		{
+			this.StartRuntime();
+		}
 
-        protected IEventAggregator EventAggregator
-        {
-            get
-            {
-                return this.eventAggregator ?? (this.eventAggregator = this.container.GetInstance<IEventAggregator>());
-            }
-        }
+		protected IEventAggregator EventAggregator
+		{
+			get
+			{
+				return this.eventAggregator ?? (this.eventAggregator = this.container.GetInstance<IEventAggregator>());
+			}
+		}
 
-        protected override void Configure()
-        {
-            container = new SimpleContainer();
-            container.Singleton<IWindowManager, WindowManager>();
-            container.Singleton<IEventAggregator, EventAggregator>();
-            container.PerRequest<IShell, ShellViewModel>();
-            container.PerRequest<IFrameViewModel, FrameViewModel>();
-            container.PerRequest<IWelcomeViewModel, WelcomeViewModel>();
-        }
+		protected override void Configure()
+		{
+			container = new SimpleContainer();
+			container.Singleton<IWindowManager, WindowManager>();
+			container.Singleton<IEventAggregator, EventAggregator>();
+			container.PerRequest<IShell, ShellViewModel>();
+			container.PerRequest<IFrameViewModel, FrameViewModel>();
+			container.PerRequest<IWelcomeViewModel, WelcomeViewModel>();
+		}
 
-        protected override object GetInstance(Type service, string key)
-        {
-            var instance = container.GetInstance(service, key);
-            if (instance != null)
-                return instance;
+		protected override object GetInstance(Type service, string key)
+		{
+			var instance = container.GetInstance(service, key);
+			if (instance != null)
+			{
+				return instance;
+			}
 
-            throw new InvalidOperationException("Could not locate any instances.");
-        }
+			throw new InvalidOperationException("Could not locate any instances.");
+		}
 
-        protected override IEnumerable<Assembly> SelectAssemblies()
-        {
-            var assemblies = base.SelectAssemblies().ToList();
+		protected override IEnumerable<Assembly> SelectAssemblies()
+		{
+			var assemblies = base.SelectAssemblies().ToList();
 
-            // This includes the Frontend.Core assembly.
-            assemblies.Add(Assembly.GetAssembly(typeof(FrameViewModel)));
+			// This includes the Frontend.Core assembly.
+			assemblies.Add(Assembly.GetAssembly(typeof(FrameViewModel)));
 
-            return assemblies;
-        }
+			return assemblies;
+		}
 
-        protected override IEnumerable<object> GetAllInstances(Type service)
+		protected override IEnumerable<object> GetAllInstances(Type service)
 		{
 			return container.GetAllInstances(service);
 		}
@@ -71,14 +74,14 @@ namespace Frontend.Client
 		{
 			DisplayRootViewFor<IShell>();
 
-            var logEntry = new LogEntry("Startup complete: Application was started from", LogEntrySeverity.Info, LogEntrySource.UI, Environment.CurrentDirectory);
+			var logEntry = new LogEntry("Startup complete: Application was started from", LogEntrySeverity.Info, LogEntrySource.UI, Environment.CurrentDirectory);
 
-            this.EventAggregator.PublishOnUIThread(logEntry);
+			this.EventAggregator.PublishOnUIThread(logEntry);
 		}
 
-        protected override void OnUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
-        {
-            this.EventAggregator.PublishOnUIThread(new LogEntry("Error occured: " + e.Exception.Message, LogEntrySeverity.Error, LogEntrySource.UI));
-        }
-    }
+		protected override void OnUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+		{
+			this.EventAggregator.PublishOnUIThread(new LogEntry("Error occured: " + e.Exception.Message, LogEntrySeverity.Error, LogEntrySource.UI));
+		}
+	}
 }
