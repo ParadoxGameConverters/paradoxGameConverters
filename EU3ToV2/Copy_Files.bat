1bf0a8736776f5f38d8fb90520179e556087d28d
idhrendur <idhrendur@hotmail.com>
2015-03-22 19:22:12 -0700

    Remove TESB influence from base converter
    Former-commit-id: 569ba94ea9c24468c4364d02a7b44a672a9357a3

--- EU3ToV2/Copy_Files.bat
+++ EU3ToV2/Copy_Files.bat
@@ -1,9 +1,9 @@
 copy "data files\country_mappings.txt" "release\country_mappings.txt"
 copy "data files\readme.txt" "release\readme.txt"
-copy "data files\province_mappings - TESB.txt" "release\province_mappings.txt"
-copy "data files\cultureMap - TESB.txt" "release\cultureMap.txt"
-copy "Data_Files\slaveCultureMap.txt" "release\slaveCultureMap.txt"
-copy "data files\religionMap - TESB.txt" "release\religionMap.txt"
+copy "data files\province_mappings.txt" "release\province_mappings.txt"
+copy "data files\cultureMap.txt" "release\cultureMap.txt"
+copy "data files\slaveCultureMap.txt" "release\slaveCultureMap.txt"
+copy "data files\religionMap.txt" "release\religionMap.txt"
 copy "data files\unions.txt" "release\unions.txt"
 copy "data files\governmentMapping.txt" "release\governmentMapping.txt"
 copy "data files\port_whitelist.txt" "release\port_whitelist.txt"
@@ -12,21 +12,57 @@ copy "data files\starting_factories.txt" "release\starting_factories.txt"
 copy "data files\merge_nations.txt" "release\merge_nations.txt"
 copy "data files\configuration.txt" "release\configuration.txt"
 copy "data files\blocked_tech_schools.txt" "release\blocked_tech_schools.txt"
-copy "Data_Files\leader_traits.txt" "release\leader_traits.txt
-copy "Data_Files\regiment_costs.txt" "release\regiment_costs.txt
-copy "Data_Files\license.txt" "release\license.txt"
+copy "data files\leader_traits.txt" "release\leader_traits.txt
+copy "data files\regiment_costs.txt" "release\regiment_costs.txt
+copy "data files\license.txt" "release\license.txt"
 
 hg log --template "Change:\t\t{rev}: {node}\nAuthor:\t\t{author}\nDescription:\t{desc}\nDate:\t\t{date|isodate}\nBranch:\t\t{branch}\n***\n" > Release/log.txt
 (for /f "delims=" %%i in (release/log.txt) do @echo %%i)>release/changelog.txt
 del release\log.txt
 
 del "Release\blankMod" /Q
-xcopy "Data Files\blankMod - TESB" "release\blankmod" /Y /E /I
+mkdir "release\blankMod"
+mkdir "release\blankMod\output"
+mkdir "release\blankMod\output\history"
+mkdir "release\blankMod\output\history\provinces"
+mkdir "release\blankMod\output\history\provinces\africa"
+mkdir "release\blankMod\output\history\provinces\asia"
+mkdir "release\blankMod\output\history\provinces\australia"
+mkdir "release\blankMod\output\history\provinces\austria"
+mkdir "release\blankMod\output\history\provinces\balkan"
+mkdir "release\blankMod\output\history\provinces\canada"
+mkdir "release\blankMod\output\history\provinces\carribean"
+mkdir "release\blankMod\output\history\provinces\central asia"
+mkdir "release\blankMod\output\history\provinces\china"
+mkdir "release\blankMod\output\history\provinces\france"
+mkdir "release\blankMod\output\history\provinces\germany"
+mkdir "release\blankMod\output\history\provinces\india"
+mkdir "release\blankMod\output\history\provinces\indonesia"
+mkdir "release\blankMod\output\history\provinces\italy"
+mkdir "release\blankMod\output\history\provinces\japan"
+mkdir "release\blankMod\output\history\provinces\low countries"
+mkdir "release\blankMod\output\history\provinces\mexico"
+mkdir "release\blankMod\output\history\provinces\pacific island"
+mkdir "release\blankMod\output\history\provinces\portugal"
+mkdir "release\blankMod\output\history\provinces\scandinavia"
+mkdir "release\blankMod\output\history\provinces\south america"
+mkdir "release\blankMod\output\history\provinces\soviet"
+mkdir "release\blankMod\output\history\provinces\spain"
+mkdir "release\blankMod\output\history\provinces\united kingdom"
+mkdir "release\blankMod\output\history\provinces\usa"
+mkdir "release\blankMod\output\history\countries"
+mkdir "release\blankMod\output\history\diplomacy"
+mkdir "release\blankMod\output\history\units"
+mkdir "release\blankMod\output\history\pops"
+mkdir "release\blankMod\output\history\pops\1836.1.1"
+mkdir "release\blankMod\output\history\wars"
+mkdir "release\blankMod\output\common"
 
 xcopy "Data Files\countries" "release\blankmod\output\common\countries" /Y /E /I
 xcopy "Data Files\gfx" "release\blankmod\output\gfx" /Y /E /I
 xcopy "Data Files\interface" "release\blankmod\output\interface" /Y /E /I
 xcopy "Data files\localisation" "release\blankmod\output\localisation" /Y /E /I
+xcopy "Data files\wars" "release\blankmod\output\wars" /Y /E /I
 copy "Data Files\countries.txt" "release\blankMod\output\common\countries.txt"
 copy "Data Files\country_colors.txt" "release\blankMod\output\common\country_colors.txt"
 copy "Data Files\religion.txt" "release\blankMod\output\common\religion.txt"
