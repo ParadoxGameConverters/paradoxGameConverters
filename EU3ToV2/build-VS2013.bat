b8f2582d42162ab5976e4cccd3b555d10f04a01e
idhrendur <idhrendur@hotmail.com>
2015-03-22 22:06:13 -0700

    Update test scripts
    Former-commit-id: 68c8b26b9b09102efc3bc8ca13b7e15b0b18d473

--- EU3ToV2/build-VS2013.bat
+++ EU3ToV2/build-VS2013.bat
@@ -3,16 +3,13 @@ del *.zip /q
 call "%VS120COMNTOOLS%\vsvars32.bat"
 
 del Release /q
-rmdir Release
+rmdir Release /S /Q
 
 del ReleaseIntermediate/q
 rmdir ReleaseIntermediate
 
 msbuild.exe EU3toV2Converter-VS2013.sln /p:Configuration=Release /m
 
-copy "UI\bin\Release\Converter.UI.exe" "release\Converter.UI.exe"
-copy "UI\UserPreferences.xml" "release\UserPreferences.xml"
-copy "UI\Configuration.xml" "release\Configuration.xml"
 
 hg log > Release/log.txt
 (for /f "delims=" %%i in (release/log.txt) do @echo %%i)>release/changelog.txt
@@ -20,6 +17,5 @@ del release\log.txt
 
 cd Release
 del *.pdb
-"%IZARC_LOC%" -ad Release;*
 call "%SEVENZIP_LOC%\7z.exe" a -tzip -r "..\EU3ToV2.zip" "*" -mx5
 cd ..
--- EU3ToV2/test.bat
+++ EU3ToV2/test.bat
@@ -1,14 +1,10 @@
 set i=%~n1
-set j=%~n2
 
-echo Testing %i% with the %j% configuration
+echo Testing %i%
+del "configuration.txt"
 copy "..\EU3_Saves\%i%.zip" ".\%i%.zip"
-"%SEVENZIP_LOC%\7z.exe" e -tzip "%i%.zip" "*.*" -mx5
+"%SEVENZIP_LOC%\7z.exe" x -tzip "%i%.zip" -mx5 -y
 del "%i%.zip"
 call EU3toV2Converter.exe "%i%.eu3"
 del "%i%.eu3" /q
 copy log.txt "..\testresults\%j%\%i%-Log.txt"
-del log.txt /q
-del output/%i%.mod
-del output/%i% /q
-rmdir %i%
