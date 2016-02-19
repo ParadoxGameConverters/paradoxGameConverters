b5b105be964244fab1ff1961af2f107900f757d9
Daniel Remenak <dtremenak@gmail.com>
2016-02-19 10:03:45 -0800

    Read YML province names from EU4 localization.

--- ProvinceMapper/LocalizationReader.cs
+++ ProvinceMapper/LocalizationReader.cs
@@ -11,38 +11,82 @@ namespace ProvinceMapper
         public LocalizationReader(string mapFolder, List<Province> provinceList, StatusUpdate su)
         {
             DirectoryInfo locFolder = new DirectoryInfo(Path.Combine(mapFolder, "..\\Localisation"));
+
+            // first, check for the new YML localization (EUIV)
+            FileInfo[] ymlFile = locFolder.GetFiles("prov_names_l_english.yml");
+            if (ymlFile.Count() == 1)
+            {
+                ReadYML(ymlFile[0], provinceList, su);
+                return;
+            }
+
+            // no relevant YML...read all the CSVs instead
             FileInfo[] locFiles = locFolder.GetFiles("*.csv");
             int fileIndex = 0;
             double fileFactor = 1.0 / locFiles.Count();
             foreach (FileInfo fi in locFiles)
             {
-                StreamReader sr = new StreamReader(fi.FullName, Encoding.GetEncoding(1252));
-                sr.ReadLine(); // discard first line
-                while (!sr.EndOfStream)
+                ReadCSV(fi, provinceList, su, fileFactor, fileIndex);
+                ++fileIndex;
+            }
+        }
+
+        private void ReadYML(FileInfo File, List<Province> provinceList, StatusUpdate su)
+        {
+            StreamReader sr = new StreamReader(File.FullName, Encoding.UTF8);
+            sr.ReadLine(); // discard first line
+            while (!sr.EndOfStream)
+            {
+                string locEntry = sr.ReadLine();
+                string[] locTokens = locEntry.Split(':');
+                if (locTokens[0].StartsWith(" PROV"))
                 {
-                    string locEntry = sr.ReadLine();
-                    string[] locTokens = locEntry.Split(';');
-                    if (locTokens[0].StartsWith("PROV"))
+                    int provNum = 0;
+                    if (int.TryParse(locTokens[0].Substring(5), out provNum))
                     {
-                        int provNum = 0;
-                        if (int.TryParse(locTokens[0].Substring(4), out provNum))
+                        foreach (Province p in provinceList)
                         {
-                            foreach (Province p in provinceList)
+                            if (p.ID == provNum)
                             {
-                                if (p.ID == provNum)
-                                {
-                                    p.name = locTokens[1];
-                                    break;
-                                }
+                                p.name = locTokens[1].Trim().Trim('\"');
+                                break;
                             }
                         }
                     }
-
-                    su(100.0 * fileFactor * (fileIndex + (double)sr.BaseStream.Position / sr.BaseStream.Length));
                 }
-                sr.Close();
-                ++fileIndex;
+
+                su(100.0 * ((double)sr.BaseStream.Position / sr.BaseStream.Length));
             }
+            sr.Close();
+        }
+
+        private void ReadCSV(FileInfo File, List<Province> provinceList, StatusUpdate su, double fileFactor, int fileIndex)
+        {
+            StreamReader sr = new StreamReader(File.FullName, Encoding.GetEncoding(1252));
+            sr.ReadLine(); // discard first line
+            while (!sr.EndOfStream)
+            {
+                string locEntry = sr.ReadLine();
+                string[] locTokens = locEntry.Split(';');
+                if (locTokens[0].StartsWith("PROV"))
+                {
+                    int provNum = 0;
+                    if (int.TryParse(locTokens[0].Substring(4), out provNum))
+                    {
+                        foreach (Province p in provinceList)
+                        {
+                            if (p.ID == provNum)
+                            {
+                                p.name = locTokens[1];
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                su(100.0 * fileFactor * (fileIndex + (double)sr.BaseStream.Position / sr.BaseStream.Length));
+            }
+            sr.Close();
         }
     }
 }
