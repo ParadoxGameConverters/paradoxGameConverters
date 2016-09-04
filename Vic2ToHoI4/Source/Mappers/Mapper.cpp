28024108601dc33574482ea5014e4df479622e0d
Idhrendur <idhrendur@hotmail.com>
2016-09-04 10:47:05 -0700

    Remove a useless reference from mapper.cpp

--- Vic2ToHoI4/Source/Mappers/Mapper.cpp
+++ Vic2ToHoI4/Source/Mappers/Mapper.cpp
@@ -26,7 +26,6 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
 #include "../Configuration.h"
 #include "Object.h"
 #include "../V2World/V2World.h"
-#include "../V2World/V2Province.h"
 #include <algorithm>
 
 
