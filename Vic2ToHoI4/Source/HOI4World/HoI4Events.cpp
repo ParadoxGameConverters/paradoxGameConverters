4db172b88de00d00d29328cf72835588e6d6eb14
Hamiller78 <Kneesch@gmx.de>
2017-06-07 21:31:01 +0200

    Fixed conversion of alliances into starting factions. Alliance with... focus in fascist tree now dismantles a joining country's faction if that country happened to be the leader of its own faction.

--- Vic2ToHoI4/Source/HOI4World/HoI4Events.cpp
+++ Vic2ToHoI4/Source/HOI4World/HoI4Events.cpp
@@ -107,7 +107,7 @@ void HoI4Events::createFactionEvents(const HoI4Country* Leader, HoI4Country* new
 	nfEvent.major = false;
 
 	string yesOption = "		name = \"Yes\"\n";
-	if (newAlly->getFaction() != nullptr)
+	/* if (newAlly->getFaction() != nullptr)
 	{
 		for (auto member: newAlly->getFaction()->getMembers())
 		{
@@ -122,6 +122,19 @@ void HoI4Events::createFactionEvents(const HoI4Country* Leader, HoI4Country* new
 			yesOption += "			add_to_faction = " + member->getTag() + "\n";
 			yesOption += "		}\n";
 		}
+	} */
+	if (Leader->getFaction() != nullptr)
+	{
+		yesOption += "		" + newAlly->getTag() + " = {\n";
+		yesOption += "			add_ai_strategy = {\n";
+		yesOption += "				type = alliance\n";
+		yesOption += "				id = \"" + Leader->getTag() + "\"\n";
+		yesOption += "				value = 200\n";
+		yesOption += "			}\n";
+		yesOption += "		}\n";
+		yesOption += "		" + Leader->getTag() + " = {\n";
+		yesOption += "			add_to_faction = " + newAlly->getTag() + "\n";
+		yesOption += "		}\n";
 	}
 	yesOption += "		hidden_effect = {\n";
 	yesOption += "			news_event = { id = news." + to_string(newsEventNumber) + " }\n";
