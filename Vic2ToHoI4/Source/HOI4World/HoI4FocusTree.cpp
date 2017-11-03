38a169f1ce653230fef3e571350c444b97b8d4dd
Johuotar <jmh@dnainternet.net>
2017-11-03 14:42:00 +0200

    Unity focus buff, new idea & few localizations
    National unity focuses now give 5%. Limited intervention gives idea that reduces tension requirement before one can send volunteers or lend lease. PP bonus was moved to prepare intervention to improve that focus. Absolutists majors had focus that did nothing so now it gives one time industry research bonus. Added English localizations to a few monarchy related ideas.

--- Vic2ToHoI4/Source/HOI4World/HoI4FocusTree.cpp
+++ Vic2ToHoI4/Source/HOI4World/HoI4FocusTree.cpp
@@ -2173,20 +2173,21 @@ void HoI4FocusTree::addDemocracyNationalFocuses(const HoI4Country* Home, const v
 	newFocus->yPos = 0;
 	newFocus->cost = 10;
 	newFocus->aiWillDo += "			factor = 10";
-	newFocus->completionReward += "			add_national_unity = 0.02";
+	newFocus->completionReward += "			add_national_unity = 0.05";
 	focuses.push_back(newFocus);
 
 	//Prepare Intervention
 	newFocus = new HoI4Focus;
 	newFocus->id = "PrepInter" + Home->getTag();
 	newFocus->icon = "GFX_goal_generic_occupy_states_ongoing_war";
-	newFocus->text += "War Propaganda";
+	newFocus->text += "Prepare Intervention";
 	newFocus->prerequisites.push_back("focus = WarProp" + Home->getTag());
 	newFocus->available += "			threat > " + to_string(0.3 * WTModifier);
 	newFocus->xPos = nextFreeColumn;
 	newFocus->yPos = 1;
 	newFocus->cost = 10;
 	newFocus->aiWillDo += "			factor = 10";
+	newFocus->completionReward += "			add_political_power = 120";
 	newFocus->completionReward += "			set_rule = { can_send_volunteers = yes }";
 	focuses.push_back(newFocus);
 
@@ -2210,7 +2211,7 @@ void HoI4FocusTree::addDemocracyNationalFocuses(const HoI4Country* Home, const v
 	newFocus->yPos = 3;
 	newFocus->cost = 10;
 	newFocus->aiWillDo += "			factor = 10";
-	newFocus->completionReward += "			add_political_power = 120";
+	newFocus->completionReward += "			add_ideas = limited_interventionism";
 	focuses.push_back(newFocus);
 
 	nextFreeColumn += 2;
@@ -2321,7 +2322,7 @@ void HoI4FocusTree::addAbsolutistEmpireNationalFocuses(const HoI4Country* Home, 
 	newFocus->aiWillDo += "				factor = 0\n";
 	newFocus->aiWillDo += "				date < 1937.6.6\n";
 	newFocus->aiWillDo += "			}";
-	newFocus->completionReward += "			add_national_unity = 0.02";
+	newFocus->completionReward += "			add_national_unity = 0.05";
 	focuses.push_back(newFocus);
 
 	//Colonies Focus
@@ -2716,7 +2717,12 @@ void HoI4FocusTree::addAbsolutistEmpireNationalFocuses(const HoI4Country* Home, 
 	newFocus->yPos = 2;
 	newFocus->cost = 10;
 	newFocus->aiWillDo += "			factor = 10";
-	//newFocus->completionReward += "			research_time_factor = -0.1;
+	newFocus->completionReward += "			add_tech_bonus = {\n";
+	newFocus->completionReward += "				name = industrial_bonus\n";
+	newFocus->completionReward += "				bonus = 0.5\n";
+	newFocus->completionReward += "				uses = 1\n";
+	newFocus->completionReward += "				category = industry\n";
+	newFocus->completionReward += "			}";
 	focuses.push_back(newFocus);
 
 	//National Highway
