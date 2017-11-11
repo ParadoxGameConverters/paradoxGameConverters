7b82a2dbc10a7a495a3fc350041e245dc6e1f983
Idhrendur <idhrendur@hotmail.com>
2017-11-11 11:59:03 -0800

    Make the province renaming event triggered in on_startup #395

--- Vic2ToHoI4/Source/HOI4World/HoI4World.cpp
+++ Vic2ToHoI4/Source/HOI4World/HoI4World.cpp
@@ -1978,6 +1978,30 @@ void HoI4World::outputOnActions() const
 	}
 	onActionsFile << "		}\n";
 	onActionsFile << "	}\n";
+
+	onActionsFile << "	on_startup = {\n";
+	onActionsFile << "		effect = {\n";
+	onActionsFile << "			set_province_name = { id = 587 name = \"Köln\"} #Cologne\n";
+	onActionsFile << "			set_province_name = { id = 957 name = \"Vladivostok\"} #Haishenwai\n";
+	onActionsFile << "			set_province_name = { id = 1025 name = \"Kokura\"} #Fukuoka\n";
+	onActionsFile << "			set_province_name = { id = 1047 name = \"Guangzhou\"} #Canton\n";
+	onActionsFile << "			set_province_name = { id = 1182 name = \"Tokyo\"} #Edo\n";
+	onActionsFile << "			set_province_name = { id = 1440 name = \"San Juan\"} #Puerto Rico\n";
+	onActionsFile << "			set_province_name = { id = 1843 name = \"Miami\"} #Tampa\n";
+	onActionsFile << "			set_province_name = { id = 3151 name = \"Leningrad\"} #Saint Petersburg\n";
+	onActionsFile << "			set_province_name = { id = 3152 name = \"Tallinn\"} #Reval\n";
+	onActionsFile << "			set_province_name = { id = 3529 name = \"Stalingrad\"} #Tsaritsyn\n";
+	onActionsFile << "			set_province_name = { id = 4180 name = \"Honolulu\"} #Hawaii\n";
+	onActionsFile << "			set_province_name = { id = 4268 name = \"Nouméa\"} #New Caledonia\n";
+	onActionsFile << "			set_province_name = { id = 4333 name = \"Astana\"} #Qaraganda\n";
+	onActionsFile << "			set_province_name = { id = 4709 name = \"Ürümqi\"} #Díhuà\n";
+	onActionsFile << "			set_province_name = { id = 4801 name = \"Ulaanbaatar\"} #Urga\n";
+	onActionsFile << "			set_province_name = { id = 6115 name = \"Oslo\"} #Christiania\n";
+	onActionsFile << "			set_province_name = { id = 7371 name = \"Kuching\"} #Brunei\n";
+	onActionsFile << "			set_province_name = { id = 11437 name = \"Dnipropetrovsk\"} #Ekaterinoslav\n";
+	onActionsFile << "			set_province_name = { id = 12674 name = \"Reykjavik\"} #Iceland\n";
+	onActionsFile << "		}\n";
+	onActionsFile << "	}\n";
 	onActionsFile << "}\n";
 	onActionsFile.close();
 }
