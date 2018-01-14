dc91f392f710e4ce2db3d10c94cb0dcfa5d02802
kratostatic <phillip.t.clarke@ntlworld.com>
2018-01-14 23:59:31 +0100

    Minor file fix

--- EU4toV2/Source/Configuration.cpp
+++ EU4toV2/Source/Configuration.cpp
@@ -1,3 +1,4 @@
+/*Copyright (c) 2017 The Paradox Game Converters Project
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
